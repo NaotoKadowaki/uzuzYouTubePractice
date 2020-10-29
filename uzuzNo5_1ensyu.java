5ca5004e3b5888599dd1497de853a567dccb2b27
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-29 21:49:46 +0900

    ＃５の演習宿題解答

---
+++ uzuzNo5_1ensyu.java
@@ -0,0 +1,20 @@
+public class uzuzNo5_1ensyu {
+    public static void main (String[] args) {
+        int lifetimeWage = 262870000 ;
+        System.out.println("生涯賃金："+ lifetimeWage + "円");
+
+        long  marketSize = 12505000000000L ;
+        System.out.println("市場規模："+ marketSize + "円");
+
+        char oneCharacter =  '極';
+        System.out.println("好きな一文字：" + oneCharacter);
+
+        String dreamOccupation = "パイロット";
+        System.out.println("なりたい職業:" + dreamOccupation);
+
+        boolean university = false ;
+        System.out.println("大卒なのか？：" + university);
+        
+       
+    }
+}
