281c4e933a6350d9adb0e6f2579423a3639c15e1
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-29 20:00:20 +0900

    youtube#5

---
+++ uzuzNo5.java
@@ -0,0 +1,9 @@
+public class uzuzNo5 {
+     public static void main (String[] args) {
+
+        int teika = 1000;
+        double zeiritu = 1.08;
+        double siharai = teika * zeiritu;
+        System.out.println(siharai);
+    }
+}
