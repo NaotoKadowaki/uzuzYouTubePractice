1cb9db105268be315c4a9fc53fb000bf966ec292
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-29 21:08:57 +0900

    変数の色々な型の種類

---
+++ uzuzNo5_1.java
@@ -0,0 +1,32 @@
+public class uzuzNo5_1 {
+    public static void main (String[] args) {
+        byte month = 12;
+        System.out.println("月" + month);
+
+        short year = 2020;
+        System.out.println("年" + year);
+
+        int income = 5000000;
+        System.out.println("年収"+ income);
+
+        long malePopulation = 3500000000L ;
+        System.out.println("人工（男性）:"  + malePopulation) ;
+
+        double acceptanceRateIT = 6.05;
+        System.out.println("IT業界の求人倍率（２０１８）：" + acceptanceRateIT);
+
+        float tax = 1.08F;
+        System.out.println("消費税:" + tax);
+
+        char weekday ='土';
+        System.out.println("曜日:"+ weekday);
+
+        String name = "もこ";
+        System.out.println("名前:" + name);
+
+        boolean genderMale = false;
+        System.out.println("性別が男性：" + genderMale);
+
+
+    }
+}
