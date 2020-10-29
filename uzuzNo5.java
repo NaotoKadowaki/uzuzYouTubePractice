9ff6cd6fea151619b530ee60cde494b477a49213
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-29 21:08:32 +0900

    no message

--- uzuzNo5.java
+++ uzuzNo5.java
@@ -1,9 +1,13 @@
+
+
 public class uzuzNo5 {
      public static void main (String[] args) {
 
         int teika = 1000;
-        double zeiritu = 1.08;
+        final double zeiritu = 1.08;
        double siharai = teika * zeiritu;
         System.out.println(siharai);
     }
 }
+
+
