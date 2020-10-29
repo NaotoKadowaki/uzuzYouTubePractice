f34468c124a7b01f62d156990fa1ce9e90e7a0ae
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-29 18:56:49 +0900

    #3 一旦練習file

---
+++ uzuzNo3.java
@@ -0,0 +1,11 @@
+class uzuzNo3 {
+    public static void main (String[] args) {
+        System.out.println("hwlloworld!!");
+    }
+}
+class uzuzu {
+public static void main (String[] args) {
+    System.out.println("sekai");
+}
+} 
+//ここまでは一旦練習
