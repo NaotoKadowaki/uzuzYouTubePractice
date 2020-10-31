1c7775ec26753cb37d5f93b9d307cb30d378bd8c
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-10-31 20:35:50 +0900

    問題集

---
+++ uzNo6_ensyu.java
@@ -0,0 +1,25 @@
+public class uzNo6_ensyu {
+    public static void main (String[]agrs){
+        int answer1 = 3+(7%4)/2;
+        System.out.println(answer1);
+
+        int calc2 = 10 ;
+        int answer2 = ++calc2;
+        System.out.println(calc2);
+        System.out.println(answer2);
+
+        int calc3 = 10;
+        int answer3 = calc3++;
+        System.out.println(calc3);
+        System.out.println(answer3);
+
+        int calc4 = 10;
+        int answer4 = calc4+1;
+        System.out.println(calc4);
+        System.out.println(answer4);
+
+        System.out.println("1"+"6");
+
+
+    }
+}
