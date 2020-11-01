72cd4838ee785f639a78b54653cc808a199132bf
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-11-01 16:26:24 +0900

    No7終了

---
+++ uzNo7.java
@@ -0,0 +1,15 @@
+public class uzNo7 {
+    public static void main (String[]args){
+        // int num = 10;
+        // System.out.println(num);
+        // String x ="10";
+        // int y = Integer.parseInt(x);
+        // System.out.println(y+num);
+        
+        int x = 10;
+        String y = String.valueOf(x);
+        System.out.println(y+"yosakoi");
+
+
+    }
+}
